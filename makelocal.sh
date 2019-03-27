3e52208c5d102ad20551dd4fcdce9204fb699c48
Ashish Pandhi <ashish@bettr.io>
2019-03-27 11:08:04 -0400

    Update Build Script

--- makelocal.sh
+++ makelocal.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-DEST=~/Applications/Autumn.app
 set -ex
-osascript -e 'quit app "Autumn"'
 xcodebuild clean build
-trash $DEST || true
-mv build/Release/Autumn.app ~/Applications
-open $DEST
